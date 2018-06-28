9541855cd2a602806dc5f37eff69d315dae0f378
Cassiano Jaeger Stradolini <cassianojaeger@gmail.com>
2018-06-27 21:37:29 -0300

    adding modal do setup artyom if selected

--- functions.js
+++ functions.js
@@ -284,7 +284,7 @@ function setupArtyom() {
 $(document).ready(function () {
     createAllMusicButtons();
     addAnimationEventToMusicButtons();
-    setupArtyom();
+    $('#warning-modal').click();
 });
 
 function isGenreInList(sGenre) {
